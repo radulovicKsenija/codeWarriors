6b73fe8503812fbe50282691030339c30ed7a7a9
Xenia Radulovic <radulovic.xeniaa@gmail.com>
2022-04-18 16:54:16 +0200

    adding first challenge

---
+++ spinWords/spinwords.js
@@ -0,0 +1,17 @@
+function spinWords(someSentence) {
+
+    var sentence = "";
+    var separate = someSentence.split(" ");
+    console.log(separate);
+    
+    for (var i = 0; i < separate.length; i++) {
+      if (sentence) sentence += ' ';
+      console.log(sentence);
+      if (separate[i].length >= 5) {
+        sentence += separate[i].split("").reverse().join("");
+      } else {
+        sentence += separate[i];
+      }
+    }
+    return sentence;
+  }
